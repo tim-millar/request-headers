2cb64b7181303c36c7d8b6164d812bdcdb13ffbc
Tim Millar <heteroskedasticity99@gmail.com>
2016-10-30 00:08:50 +0100

    Tidying up

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ group :development do
 end
 
 group :test do
-  gem 'cucumber-rails', require: false
   gem 'database_cleaner'
   gem 'vcr'
 end
--- spec/features/headers/show_spec.rb
+++ spec/features/headers/show_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'show headers', type: :request do
   it 'shows the headers from a browser request' do
-    get '/', request
+    get '/'
 
+    expect(response.content_type.to_s).to eql('application/json')
     expect(response.body).to include('ipaddress')
     expect(response.body).to include('language')
     expect(response.body).to include('software')
